439d1b70b6b3268182750baf01dd7ee9a4e217c4
Luke Towers <git@luketowers.ca>
2025-06-25 17:45:00 -0600

    Create _basic_popup.php

---
+++ controllers/records/_basic_popup.php
@@ -0,0 +1,17 @@
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="popup">&times;</button>
+    <h4 class="modal-title">Popup Title</h4>
+</div>
+<div class="modal-body">
+    <p>
+        Popup Contents
+    </p>
+</div>
+<div class="modal-footer">
+    <button
+        type="button"
+        class="btn btn-default"
+        data-dismiss="popup">
+        <?= e(trans('backend::lang.form.close')) ?>
+    </button>
+</div>
